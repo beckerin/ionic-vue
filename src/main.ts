380b070d5e09a413a30986397f7ef4f208c0b1a2
Diego Paiva <paivadiego7797@gmail.com>
2026-02-14 00:48:34 +0000

    base

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
@@ -13,12 +13,12 @@ import '@ionic/vue/css/structure.css';
 import '@ionic/vue/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/display.css';
+import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/padding.css';
 import '@ionic/vue/css/text-alignment.css';
 import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
 
 /**
  * Ionic Dark Mode
@@ -27,9 +27,9 @@ import '@ionic/vue/css/display.css';
  * https://ionicframework.com/docs/theming/dark-mode
  */
 
-/* @import '@ionic/vue/css/palettes/dark.always.css'; */
+import '@ionic/vue/css/palettes/dark.always.css';
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
-import '@ionic/vue/css/palettes/dark.system.css';
+/* import '@ionic/vue/css/palettes/dark.system.css'; */
 
 /* Theme variables */
 import './theme/variables.css';
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,28 +1,21 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header class="ion-no-border">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-button router-link="/dark" router-direction="forward">Go to detail</ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 </script>
 
 <style scoped>
